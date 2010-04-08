3c6fb3c00d5d9023c21d021b019bde294a08562e
Macpiece <macpiece@Macpieces-MacBook.local>
2010-04-08 12:23:25 -0700

    moved tests for product options to store

--- cucumber/step_definitions/product_options.rb
+++
@@ -1,9 +0,0 @@
-Given /^the following (.+) records$/ do |factory, table|
-  table.hashes.each do |hash|
-    Factory(factory, hash)
-  end
-end
-
-Then /^I should see "([^\"]*)" and "([^\"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
--- cucumber/step_definitions/video_steps.rb
+++
@@ -1,5 +0,0 @@
-Given /^I have videos named (.+)$/ do |video|
-  video.split(", ").each do |name|
-    Product.create!(:name => name)
-  end
-end
