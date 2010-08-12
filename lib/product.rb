776cc539880eee19f5fb6f2b9aff4f06608e6929
Ameravant <contact@ameravant.com>
2010-08-12 15:53:12 -0700

    formatting modifications to products and search

--- lib/product.rb
+++ lib/product.rb
@@ -1,8 +1,7 @@
 class Product < ActiveRecord::Base
-  require "#{RAILS_ROOT}/vendor/plugins/siteninja/siteninja_store/app/models/product.rb"
-  acts_as_taggable
   named_scope :video, :conditions => ["is_video = ?", 't']
   named_scope :active, :conditions => ["active = ?", 't']
+  require "#{RAILS_ROOT}/vendor/plugins/siteninja/siteninja_store/app/models/product.rb"
   def self.videos_tagged_with(tag)
     active.video.find_tagged_with(tag)
   end
