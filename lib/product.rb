f388f30d4a67ef2d0e5c93bfff94ab4f5d7752c1
jason gagne <jgagne33@gmail.com>
2010-08-31 11:20:30 -0700

    cleaning things up

--- lib/product.rb
+++ lib/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
-  named_scope :video, :conditions => ["is_video = ?", 't']
-  named_scope :active, :conditions => ["active = ?", 't']
+  named_scope :video, :conditions => {:is_video => true}
+  named_scope :active, :conditions => {:active => true}
   require "#{RAILS_ROOT}/vendor/plugins/siteninja/siteninja_store/app/models/product.rb"
   def self.videos_tagged_with(tag)
     active.video.find_tagged_with(tag)
