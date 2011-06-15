39a2d8d670c03d707aa8df6e324a347d05066d6d
Ameravant <contact@ameravant.com>
2011-06-15 15:55:58 -0700

    added add multiple images route to products

--- routes.rb
+++ routes.rb
@@ -3,7 +3,11 @@ resources :product_categories, :has_many => :images do |cat|
 end
 namespace :admin do |admin|
   admin.resources :product_categories, :has_many => :images, :collection => { :reorder => :put }, :member => { :reorder => :put } do |cat|
-    cat.resources :products
+    cat.resources :products, :has_many => :features do |product|
+      product.resources :images, :collection => { :reorder => :put, :add_multiple => :get }
+      product.resources :testimonials
+    end
+
     cat.resources :menus
   end
 end
